f0632cbe3bc51999030bf1983315ef9c80e27c7d
mcproger <artem7lomonosov@gmail.com>
2017-08-02 16:57:10 +0300

    Update enter of filepath

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,7 +3,7 @@ import string, os
 
 def load_data(filepath):
     if not os.path.exists(filepath):
-        return None
+        return False
     with open(filepath, 'r', encoding='utf-8') as myfile:
     	text = myfile.read()
     	return text
@@ -24,12 +24,14 @@ def get_most_frequent_words(text):
 
 if __name__ == '__main__':
     filepath = input('Enter path to file with text: ')
-    text = load_data(filepath).split()
-    text_without_punct = delete_punctuation_from_text(text)
-    most_frequent_words = get_most_frequent_words(text_without_punct)
-    for words in most_frequent_words:
-        print(words[0], words[1])
-    
+    if load_data(filepath):
+        text = load_data(filepath).split()
+        text_without_punct = delete_punctuation_from_text(text)
+        most_frequent_words = get_most_frequent_words(text_without_punct)
+        for words in most_frequent_words:
+            print(words[0], words[1])
+    else:
+        print('No such file in directory')
 
 
 
