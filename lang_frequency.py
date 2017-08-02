1eef362094d71cfd9a2db8dcf84f6971437ba10d
mcproger <artem7lomonosov@gmail.com>
2017-08-02 16:30:17 +0300

    Add get most frequent words functional

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,38 @@
+import string, os
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r', encoding='utf-8') as myfile:
+    	text = myfile.read()
+    	return text
+
+
+def delete_punctuation_from_text(text):
+	text = [word.replace(string.punctuation, '') for word in text]
+	return text
 
 
 def get_most_frequent_words(text):
-    pass
+    frequent_words = {}    
+    for word in text:
+    	frequent_words[word.capitalize()] = frequent_words.get(word.capitalize(), 0) + 1
+    sorted_frequent_words = sorted(frequent_words.items(), key=lambda item: item[1], reverse=True)
+    return sorted_frequent_words[:10]
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input('Enter path to file with text: ')
+    text = load_data(filepath).split()
+    text_without_punct = delete_punctuation_from_text(text)
+    most_frequent_words = get_most_frequent_words(text_without_punct)
+    for words in most_frequent_words:
+        print(words[0], words[1])
+    
+
+
+
+
+
+
