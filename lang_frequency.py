e3a1008dbfd4f9fed63be111e8d7702b991f3643
mcproger <artem7lomonosov@gmail.com>
2017-08-13 16:48:21 +0300

    Update with using Counter

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,40 +1,41 @@
-import string, os
+import string
+import os
+import collections
 
 
 def load_data(filepath):
     if not os.path.exists(filepath):
-        return False
+        return None
     with open(filepath, 'r', encoding='utf-8') as myfile:
-    	text = myfile.read()
-    	return text
+        text = myfile.read()
+        return text
 
 
 def delete_punctuation_from_text(text):
-	text = [word.replace(string.punctuation, '') for word in text]
-	return text
+    text = [word.replace(string.punctuation, '') for word in text]
+    return text
+
+
+def get_words_frequency(text):
+    words_frequency = collections.Counter()
+    for word in text:
+        words_frequency[word.capitalize()] += 1
+    return words_frequency
 
 
 def get_most_frequent_words(text):
-    frequent_words = {}    
-    for word in text:
-    	frequent_words[word.capitalize()] = frequent_words.get(word.capitalize(), 0) + 1
-    sorted_frequent_words = sorted(frequent_words.items(), key=lambda item: item[1], reverse=True)
-    return sorted_frequent_words[:10]
+    filtered_text = delete_punctuation_from_text(text)
+    words_frequency = get_words_frequency(filtered_text)
+    most_frequent_words = words_frequency.most_common(10)
+    return most_frequent_words
 
 
 if __name__ == '__main__':
     filepath = input('Enter path to file with text: ')
     if load_data(filepath):
         text = load_data(filepath).split()
-        text_without_punct = delete_punctuation_from_text(text)
-        most_frequent_words = get_most_frequent_words(text_without_punct)
-        for words in most_frequent_words:
-            print(words[0], words[1])
+        most_frequent_words = get_most_frequent_words(text)
+        for word, count in most_frequent_words:
+            print(word, count)
     else:
         print('No such file in directory')
-
-
-
-
-
-
