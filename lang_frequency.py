8c8ab4804d30a087596941e4838afff55a42bf18
mcproger <artem7lomonosov@gmail.com>
2017-08-15 12:13:22 +0300

    Update enter of arguments with argparse

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,6 +1,7 @@
 import string
 import os
 import collections
+import argparse
 
 
 def load_data(filepath):
@@ -9,6 +10,16 @@ def load_data(filepath):
     with open(filepath, 'r', encoding='utf-8') as myfile:
         text = myfile.read()
         return text
+
+
+def get_argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', type=str,
+                        help='file containing text for frequency analize')
+    parser.add_argument('words_count', type=int,
+                        help='The number of words that should be displayed in the console')
+    args = parser.parse_args()
+    return args
 
 
 def delete_punctuation_from_text(text):
@@ -23,18 +34,18 @@ def get_words_frequency(text):
     return words_frequency
 
 
-def get_most_frequent_words(text):
+def get_most_frequent_words(text, words_count):
     filtered_text = delete_punctuation_from_text(text)
     words_frequency = get_words_frequency(filtered_text)
-    most_frequent_words = words_frequency.most_common(10)
+    most_frequent_words = words_frequency.most_common(words_count)
     return most_frequent_words
 
 
 if __name__ == '__main__':
-    filepath = input('Enter path to file with text: ')
-    if load_data(filepath):
-        text = load_data(filepath).split()
-        most_frequent_words = get_most_frequent_words(text)
+    args = get_argparser()
+    if load_data(args.filepath):
+        text = load_data(args.filepath).split()
+        most_frequent_words = get_most_frequent_words(text, args.words_count)
         for word, count in most_frequent_words:
             print(word, count)
     else:
